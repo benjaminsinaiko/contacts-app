31b79702c5c883f22194dec9f305145721aa4b96
benjaminsinaiko <ben.jamin@outlook.com>
2017-11-19 11:32:55 -0600

    add new model methods for full name, japan prefix, friendly update

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,25 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+    return full_name
+  end
+
+  def phone_japan
+    phone_japan = "+81 #{phone_number}"
+    return phone_japan
+  end
+
+  def as_json
+    {
+      id: id,
+      name: full_name,
+      email: email,
+      phone: phone_japan,
+      updated: friendly_updated_at
+    }
+  end
 end
