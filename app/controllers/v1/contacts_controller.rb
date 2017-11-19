f39b647b6a61ed319cae7d36c695b4107a890b28
benjaminsinaiko <ben.jamin@outlook.com>
2017-11-19 11:17:52 -0600

    Add main.rb file

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -6,17 +6,17 @@ class V1::ContactsController < ApplicationController
 
   def create
     contact = Contact.new(
-      first_name: params(:first_name),
-      last_name: params(:last_name),
-      email: params(:email),
-      phone_number: params(:phone_number)
-    )
+    first_name: params["first_name"],
+    last_name: params["last_name"],
+    email: params["email"],
+    phone_number: params["phone_number"]
+  )
     contact.save
     render json: contact.as_json
   end
 
   def show
-    contact_id = params["id"]
+    contact_id = params[:id]
     contact = Contact.find_by(id: contact_id)
     render json: contact.as_json
   end
--- main.rb
+++ main.rb
@@ -1,1 +1,59 @@
-requre "unirest"
+require "unirest"
+require "pp"
+
+puts "Welcome to the Contacts app! Select an option:"
+puts "[1] Show all contacts"
+puts "[2] Show contact by id"
+puts "[3] Create contact"
+puts "[4] Update contact by id"
+puts "[5] Delete contact by id"
+input = gets.chomp
+
+if input == "1"
+  response = Unirest.get("http://localhost:3000/v1/contacts")
+  contacts = response.body
+  pp contacts
+elsif input == "2"
+  print "Choose contact by id: "
+  contact_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contacts/#{contact_id}")
+  contact = response.body
+  pp contact
+elsif input == "3"
+  params = {}
+  print "Enter fist name: "
+  params["first_name"] = gets.chomp
+  print "Enter last name: "
+  params["last_name"] = gets.chomp
+  print "Enter email: "
+  params["email"] = gets.chomp
+  print "Enter phone number: "
+  params["phone_number"] = gets.chomp
+  response = Unirest.post("http://localhost:3000/v1/contacts", parameters: params)
+  contact = response.body
+  pp contact
+elsif input == "4"
+  print "Enter contact by id to update: "
+  contact_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contacts/#{contact_id}")
+  contact = response.body
+  params = {}
+  print "Enter updated first name (#{contact["first_name"]}): "
+  params["first_name"] = gets.chomp
+  print "Enter updated last name (#{contact["last_name"]}): "
+  params["last_name"] = gets.chomp
+  print "Enter updated email (#{contact["email"]}): "
+  params["email"] = gets.chomp
+  print "Enter updated phone number (#{contact["phone_number"]}): "
+  params["phone_number"] = gets.chomp
+  params.delete_if { |key, value| value.empty? }
+  response = Unirest.patch("http://localhost:3000/v1/contacts/#{contact_id}", parameters: params)
+  updated_contact = response.body
+  pp updated_contact
+elsif input == "5"
+  print "Choose contact by id to delete: "
+  contact_id = gets.chomp
+  response = Unirest.delete("http://localhost:3000/v1/contacts/#{contact_id}")
+  contact = response.body
+  pp contact
+end
